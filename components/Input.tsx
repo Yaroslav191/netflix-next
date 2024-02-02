c31f1de454fb7695ddf653bde80463045b29e0dc
Yaroslav <yaroslavv86@gmail.com>
2024-02-02 18:02:37 +0600

    fix

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,10 @@
 const Input = () => {
-   return <div>input</div>;
+   return (
+      <input
+         type="text"
+         className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0"
+      />
+   );
 };
 
 export default Input;
